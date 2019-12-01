e75df3f97e976e6ccb2bfb36538a17ac7b72bb85
Grzegorz Hadala <grzegorz.hadala@gmail.com>
2019-12-01 17:55:03 +0100

    fixing work gallery

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,55 +1,40 @@
 <template>
-    <div class="columnsz">
-        <!-- <div class="column is-one-third">
-            <page-info 
-                title="Work"
-                description="Some description about work">
-            </page-info>
-            <div class="sticky">
-                <ul>
-                    <li>Something 1</li>
-                    <li>Something 2</li>
-                    <li>Something 3</li>
-                    <li>Something 4</li>
-                    <li>Something 5</li>
-                </ul>
-            </div>
-        </div> -->
-        <div class="columnz is-two-thirdsz">
-            <!-- <page-info 
-                title="Work"
-                description="Some description about work">
-            </page-info> -->
-            <div class="columns is-multiline tiles">
-                <div class="column" 
-                    v-for="work in orderedWorks" 
-                    v-bind:key="work.name"
-                    v-bind:class="[work.layout ? 'is-full' : 'is-half']"
-                    >
-                    
-                        <div class="project-item">
-                            <n-link :to=work._path class="title">
-                            <figure class="mockup">
-                                <!-- Sqaure -->
-                                <!-- <img src="screen.png" alt=""> -->
-                                <img v-bind:src="work.cover" />
-                            </figure>
-                            </n-link>
-                            <div class="caption">
-                                <!-- Free -->
-                                <n-link :to=work._path class="title">{{work.title}} &rarr;</n-link>
-                                <p class="description">{{work.description}}</p>
-                                
-                            </div>
-                        </div>
-                    
+    <div class="work-grid">
+        <div class="grid-item">
+            <h1>Work</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam libero eveniet quo ullam numquam quasi laborum, reiciendis voluptate! Hic dolore sit consequatur vel accusamus facilis reprehenderit, cumque delectus incidunt quibusdam.</p>
+        </div>
+        <div class="grid-item" 
+            v-for="work in orderedWorks" 
+            v-bind:key="work.name"
+            v-bind:class="[work.layout ? 'is-full' : 'is-half']"
+            >
+            
+                <div class="project-item">
+                    <n-link :to=work._path class="title">
+                    <figure class="mockup">
+                        <!-- Sqaure -->
+                        <!-- <img src="screen.png" alt=""> -->
+                        <img v-bind:src="work.cover" />
+                    </figure>
+                    </n-link>
+                    <div class="caption">
+                        <!-- Free -->
+                        <n-link :to=work._path class="title">{{work.title}}</n-link>
+                        <p class="description">{{work.description}}</p>
+                        
+                    </div>
                 </div>
-            </div>
+            
         </div>
     </div>
 </template>
 
 <script>
+// TODO
+// - import ShowImage
+// Inject folder name
+
 
 import PageInfo from '~/components/PageInfo.vue'
 import _ from 'lodash';  
@@ -73,6 +58,41 @@ export default {
 <style lang="scss">
 @import "~/assets/variables.scss";
 
+.work-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: auto;
+    grid-gap: 4em;
+    grid-auto-flow: dense;
+    .grid-item {
+        //background: $light-gray;
+    }
+    &:first-child {
+        //grid-column: 1 / 2;
+    }
+    .is-half {
+        grid-column-end: span 1;
+        grid-row-end: span 2;
+    }
+    *.is-full {
+        grid-column-end: span 2;
+        grid-row-end: span 1;
+    }
+    @media screen and (max-width: $max-mobile) {
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2em;
+    }
+    @media screen and (max-width: 414px) {
+        display: block;
+        .grid-item {
+            &:not(:last-child) {
+                margin-bottom: 2em;
+            }
+        }
+    }
+}
+
+
 .project-item {
     display: flex;
     flex-direction: column;
@@ -81,39 +101,42 @@ export default {
 
 .tiles {
     .column {
-        &:nth-child(even) {
-            .project-item {
-                margin-top: 8em;
-            }
-        }
+        // &:nth-child(even) {
+        //     .project-item {
+        //         margin-top: 8em;
+        //     }
+        // }
+        width: calc(33% - 4em)!important;
     }
 }
 
 .mockup {
+    //max-width: 80%;
     display: flex;
     align-content: center;
     text-align: center;
     justify-content: center;
     position: relative;
+    //padding: 2vw;
     //padding: 2em 0;
     &:hover {
         img {
-            transform: scale(1);
+            transform: scale(1.025);
         }
     }
     img {
         transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
-        top: 4em;
+        //top: 4em;
         width: 100%;
         height: auto;
-        transform: scale(.95);
+        transform: scale(1);
     }
 }
 
 .caption {
     //font-size: 0.9em;
     margin-top: 1em;
-    margin-left: 2em;
+    padding: 0 1em;
     .title {
         font-weight: 600;
         //font-size: 1em;
